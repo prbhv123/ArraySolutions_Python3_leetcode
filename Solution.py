68b695ece9802cda4a398c173e197096965004af
Prabhav Prashar <80258361+prbhv123@users.noreply.github.com>
2024-12-28 20:58:37 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -103,26 +103,25 @@ class Solution:
 class Solution:
     def findUnion(self,a,b):
         i,j = 0, 0
-        union = []
+        Union = []
         while i < len(a) and j < len(b):
             if a[i] <= b[j]:
-                if len(union) == 0 or union[-1] != a[i]:
-                    union.append(a[i])
+                if len(Union) == 0 or Union[-1] != a[i]:
+                    Union.append(a[i])
                 i+=1
             else:
-                if len(union) == 0 or union[-1] != b[j]:
-                    union.append(b[j])
+                if len(Union) == 0 or Union[-1] != b[j]:
+                    Union.append(b[j])
                 j+=1
         while i < len(a):
-            if union[-1] != a[i]:
-                union.append(a[i])
+            if Union[-1] != a[i]:
+                Union.append(a[i])
             i+=1
         while j < len(b):
-            if union[-1] != b[j]:
-                union.append(b[j])
-            j+=1
-                
-        return union
+            if Union[-1] != b[j]:
+                Union.append(b[j])
+            j+=1      
+        return Union
         
 
     
